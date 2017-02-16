ae294eeece5fe64789ce0e2898582f95cf05059a
akshun4 <akshuny@gmail.com>
2017-02-16 18:17:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById("main-text");
 element.innerHTML="yeah...not really"
 
 //Move the image
-var img=document.getElementbyId("madi");
+var img=document.getElementById("madi");
 img.onclick=function(){
     img.style.marginLeft = '80px';
 }
