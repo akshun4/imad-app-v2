86a961c3301570c65a490456c6cff5c18e4fc399
akshun4 <akshuny@gmail.com>
2017-02-16 14:05:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,79 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articles={
+    articleOne:{
+        title:'Article One|Akshun',
+        heading:'Article One',
+        date:'Feb 16, 2017',
+        content:`<p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>
+                <p>
+                    This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+                </p>`
+    },
+    articleTwo:{
+        title:'Article Two|Akshun',
+        heading:'Article Two',
+        date:'Feb 16, 2017',
+        content:`<p>
+                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                </p>
+                <p>
+                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                </p>
+                <p>
+                    This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article. This is the content for my second article.
+                </p>`
+    },
+    articleThree:{
+        title:'Article Three|Akshun',
+        heading:'Article Three',
+        date:'Feb 16, 2017',
+        content:'This is the content for my third article.'
+    }
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+    var htmlTemplate=`
+        <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=devide-width, initial-scale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href ="/">Home</a>
+                </div>
+                <hr>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+        </html>
+    `;
+    
+    return htmlTemplate;
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -18,15 +91,15 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.sendFile(createTemplate(articleThree));
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
