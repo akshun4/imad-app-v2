993ab988d65d6e8ec0587ec0e20e2e91646c7e14
akshun4 <akshuny@gmail.com>
2017-02-22 19:17:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ submit.onclick = function(){
                 var names=request.responseText;
                 names=JSON.parse(names);
                 var list='';
-                for (var i=0;i<names.length();i++){
+                for (var i=0;i<names.length;i++){
                 list=list+'<li>'+names[i]+'</li>';
                 }
             var ul=document.getElementById('namelist');
