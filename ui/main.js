53f5c9ff9bf781ffd32b8bdd610ef0d369215c19
akshun4 <akshuny@gmail.com>
2017-02-22 18:27:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,17 +25,34 @@ button.onclick= function(){
 };
 
 var nameInput=document.getElementById('name');
-var name=nameInput.value();
+var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 
 submit.onclick = function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0;i<names.length();i++){
-        list+='<li>'+names[i]+'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
     
-}
+    // Make a request to the counter endpoint
+    var request=new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // everything is good, the response is received
+            
+            if (request.status === 200){
+                var names=response.requestText;
+                names=JSON.parse(names);
+                var list='';
+                for (var i=0;i<names.length();i++){
+                list+='<li>'+names[i]+'</li>';
+                }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
+            }
+        }
+    // Make the request
+    request.open('GET','http://akshun4.imad.hasura-app.io/submit-name?name='+name);
+    request.send(null);
+    };
+};
     
